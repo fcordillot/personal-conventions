12eab1d3aa3037b55d965347e198188ed771d1af
Florian Cordillot <florian.cordillot@gmail.com>
2024-09-30 22:57:08 +0200

    update(app): update readme

---
+++ README.md
@@ -0,0 +1,16 @@
+# Personal conventions
+A documentation gathering my personal conventions and practices.
+
+> ℹ️ This documentation is improved over time.
+
+## Editor config
+*   [Editor config](editorconfig/README.md)
+
+## Git
+*   [Git](git/README.md)
+
+## Vue.js
+*   [Vue.js](Vue.js/README.md)
+
+## React
+*   [React](React/README.md)
---
+++ Vue.js/vite.config.ts
@@ -0,0 +1,32 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import mkcert from 'vite-plugin-mkcert'
+import { VitePWA } from 'vite-plugin-pwa'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    mkcert(),
+    vue()
+  ],
+  build: {
+    minify: false,
+    chunkSizeWarningLimit: 1600,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString()
+          }
+        }
+      }
+    }
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
